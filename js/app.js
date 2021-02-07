8a8294400af2113a4ab86e7399c7784a01deb00a
Hasib787 <mr.hasib45@gmail.com>
2021-02-07 20:35:38 +0600

    added some style

--- js/app.js
+++ js/app.js
@@ -9,20 +9,15 @@ loadData().then(countries => {
 
 const displayCountries = countries => {
     const countriesDiv = document.getElementById('countries');
-    const allCountry = countries.map(country => country.name);
-    const allCapital = countries.map(capital => capital.capital)
-    const countryDiv= document.createElement('div');
-    const countryName= document.createElement('h3');
-    countryName.innerText=allCountry;
-    const capitalName = document.createElement('p');
-    capitalName.innerText= allCapital;
-    countryDiv.appendChild(countryName);
-    countryDiv.appendChild(capitalName);
-    countriesDiv.appendChild(countryDiv);
-
-
-    // for (let i = 0; i < countries.length; i++) {
-    //     const country = countries[i];
-    //     console.log(country.name); 
-    // }
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+        const countryDiv= document.createElement('div');
+        countryDiv.className= 'country';
+        const countryInfo = `
+            <h3 class="country-name">${country.name}</h3>
+            <P>${country.capital}</p>
+        `
+        countryDiv.innerHTML= countryInfo;
+        countriesDiv.appendChild(countryDiv);
+    }
 }
