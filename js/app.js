5ecced4dd8b921c7ab24d5ed7e618cb9b010755f
Hasib787 <mr.hasib45@gmail.com>
2021-02-07 22:02:41 +0600

    added some function

--- js/app.js
+++ js/app.js
@@ -13,10 +13,30 @@ const displayCountries = countries => {
         const countryDiv = document.createElement('div');
         countryDiv.className = 'country';
         const countryInfo = `
-            <h3 class="country-name">${country.name}</h3>
+            <h3  class="country-name">${country.name}</h3>
             <P>${country.capital}</p>
+            <button onclick="displayCountriesDetail('${country.name}')">Submit</button>
         `;
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
+
     });
 }
+
+const displayCountriesDetail = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderCountryInfo(data[0]));
+}
+
+const renderCountryInfo = country => {
+    const countryDiv = document.getElementById('countryDetail');
+    countryDiv.innerHTML = `
+        <h1>${country.name}</h1>
+        <p>Population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        <img src="${country.flag}">
+    `
+}
+
